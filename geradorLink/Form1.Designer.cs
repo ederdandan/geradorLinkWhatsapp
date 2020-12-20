325d633647be872ec98330d4e3c2b59b9a75c1fc
ederdandan <ederdandan@gmail.com>
2020-12-19 23:37:28 -0300

    primeiro commit

---
+++ geradorLink/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace geradorLink
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(48, 24);
+            this.txtTelefone.MaxLength = 15;
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(394, 45);
+            this.txtTelefone.TabIndex = 0;
+            this.txtTelefone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(48, 75);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(394, 23);
+            this.txtResultado.TabIndex = 3;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnCopiar.Enabled = false;
+            this.btnCopiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCopiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.ForeColor = System.Drawing.Color.White;
+            this.btnCopiar.Location = new System.Drawing.Point(152, 116);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(187, 44);
+            this.btnCopiar.TabIndex = 4;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = false;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(479, 172);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtTelefone);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Link Whatsapp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCopiar;
+    }
+}
+
---
+++ geradorLink/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace geradorLink
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtTelefone.LostFocus += txtTelefone_LostFocus;
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e) => Gerar();
+
+        private void Gerar()
+        {
+            var numero = ParseText(txtTelefone.Text);
+            ManipularTelefone(ref numero);
+            txtResultado.Text = GerarLink(numero);
+            btnCopiar.Enabled = true;
+        }
+
+        private string GerarLink(string numero) => "https://web.whatsapp.com/send?phone=55" + numero;
+
+        private string ParseText(string text) => string.Join("", Regex.Split(text, @"\D+"));
+
+        private string ManipularTelefone(ref string telefone)
+        {
+            if (telefone.Length <= 9)
+                telefone = "11" + telefone;
+            return telefone;
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtResultado.Text))
+                Clipboard.SetText(txtResultado.Text);
+        }
+
+        private void txtTelefone_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTelefone.Text))
+            {
+                txtResultado.Text = "";
+                btnCopiar.Enabled = false;
+            }
+            else
+                Gerar();
+        }
+    }
+}
